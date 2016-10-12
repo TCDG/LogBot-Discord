c0a8080835e4797a375c1d68f9303222feae2825
XeliteXirish <shaunoneill7@me.com>
2016-10-12 18:43:03 +0100

    Permissions

---
+++ src/main/java/com/xelitexirish/logbot/handlers/PermissionHandler.java
@@ -0,0 +1,7 @@
+package com.xelitexirish.logbot.handlers;
+
+/**
+ * Created by XeliteXirish on 12/10/2016. www.xelitexirish.com
+ */
+public class PermissionHandler {
+}
---
+++ src/main/java/com/xelitexirish/logbot/utils/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.xelitexirish.logbot.utils;
+
+/**
+ * Created by XeliteXirish on 12/10/2016. www.xelitexirish.com
+ */
+public class MessageUtils {
+}
