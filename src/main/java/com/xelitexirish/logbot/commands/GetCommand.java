4fa5d11292d0aefd967c001d20665403486b01b6
XeliteXirish <shaunoneill7@me.com>
2016-10-23 21:18:01 +0100

    New formatting for player get

--- src/main/java/com/xelitexirish/logbot/commands/GetCommand.java
+++ src/main/java/com/xelitexirish/logbot/commands/GetCommand.java
@@ -26,44 +26,14 @@ public class GetCommand implements ICommand {
 
         if (PermissionHandler.isUserAdmin(event.getGuild(), event.getAuthor())) {
             try {
-                if (args.length > 0) {
 
-                    if (args[0].equalsIgnoreCase("all")) {
-                        File[] channelFiles = FileHandler.getAllServerLogFiles(event.getGuild());
-                        if (channelFiles != null) {
-                            event.getAuthor().getPrivateChannel().sendMessage("Here are the chats logs for the server: " + event.getGuild().getName());
+                if (args.length > 0 && args[0].equalsIgnoreCase("channel")){
+                    getChannelLog(args, event);
 
-                            for (File file : channelFiles) {
-                                event.getAuthor().getPrivateChannel().sendFile(file, null);
-                                BotLogger.info(event.getAuthorName() + " asked for file: " + file.getName() + " on server: " + event.getGuild().getName());
-                            }
-                        } else {
-                            event.getAuthor().getPrivateChannel().sendMessage("Sorry but an error occurred or there was no chat logs found!");
-                        }
-
-                    } else if (event.getMessage().getMentionedChannels().size() > 0) {
-
-                        event.getAuthor().getPrivateChannel().sendMessage("Here are the chat logs for the channels you asked for:");
-                        for (TextChannel channel : event.getMessage().getMentionedChannels()) {
-                            File channelFile = FileHandler.getLogFile(event.getGuild(), channel);
-                            event.getAuthor().getPrivateChannel().sendFile(channelFile, null);
-
-
-                            BotLogger.info(event.getAuthorName() + " asked for file: " + channelFile.getName() + " on server: " + event.getGuild().getName());
-                        }
-
-                    } else {
-                        sendHelpMessage(event);
-                    }
-                } else {
-
-                    File logFile = FileHandler.getLogFile(event.getGuild(), event.getTextChannel());
-                    MessageBuilder messageBuilder = new MessageBuilder();
-                    messageBuilder.appendString("This is the log file for channel: " + event.getTextChannel().getName());
-
-                    event.getAuthor().getPrivateChannel().sendFile(logFile, messageBuilder.build());
-                    BotLogger.info(event.getAuthorName() + " asked for file: " + logFile.getName() + " on server: " + event.getGuild().getName());
+                }else if (args.length > 0 && args[0].equalsIgnoreCase("player")){
+                    getPlayerLog(args, event);
                 }
+
             } catch (RateLimitedException e) {
                 BotLogger.error("I got rate limited sending chat logs for the server: " + event.getGuild().getName());
             }
@@ -77,7 +47,7 @@ public class GetCommand implements ICommand {
 
     @Override
     public String help() {
-        return "Returns the log file for the specified channel";
+        return "Returns the log file for the specified channel. Usage: get channel <mentioned channel>";
     }
 
     @Override
@@ -88,5 +58,56 @@ public class GetCommand implements ICommand {
     @Override
     public String getTag() {
         return "get";
+    }
+
+
+    private void getChannelLog(String[] args, MessageReceivedEvent event) {
+
+        if (args.length == 1) {
+            // get channel
+
+            File logFile = FileHandler.getLogFile(event.getGuild(), event.getTextChannel());
+            MessageBuilder messageBuilder = new MessageBuilder();
+            messageBuilder.appendString("This is the log file for channel: " + event.getTextChannel().getName());
+
+            event.getAuthor().getPrivateChannel().sendFile(logFile, messageBuilder.build());
+            BotLogger.info(event.getAuthorName() + " asked for file: " + logFile.getName() + " on server: " + event.getGuild().getName());
+
+        } else if (args.length > 1){
+
+            if (args[1].equalsIgnoreCase("all")) {
+                // get channel all
+
+                File[] channelFiles = FileHandler.getAllServerLogFiles(event.getGuild());
+                if (channelFiles != null) {
+                    event.getAuthor().getPrivateChannel().sendMessage("Here are the chats logs for the server: " + event.getGuild().getName());
+
+                    for (File file : channelFiles) {
+                        event.getAuthor().getPrivateChannel().sendFile(file, null);
+                        BotLogger.info(event.getAuthorName() + " asked for file: " + file.getName() + " on server: " + event.getGuild().getName());
+                    }
+                } else {
+                    event.getAuthor().getPrivateChannel().sendMessage("Sorry but an error occurred or there was no chat logs found!");
+                }
+
+            } else if (event.getMessage().getMentionedChannels().size() > 0) {
+                // get channel #channelName
+
+                event.getAuthor().getPrivateChannel().sendMessage("Here are the chat logs for the channels you asked for:");
+                for (TextChannel channel : event.getMessage().getMentionedChannels()) {
+                    File channelFile = FileHandler.getLogFile(event.getGuild(), channel);
+                    event.getAuthor().getPrivateChannel().sendFile(channelFile, null);
+
+                    BotLogger.info(event.getAuthorName() + " asked for file: " + channelFile.getName() + " on server: " + event.getGuild().getName());
+                }
+
+            } else {
+                sendHelpMessage(event);
+            }
+        }
+    }
+
+    private void getPlayerLog(String[] args, MessageReceivedEvent event) {
+
     }
 }
