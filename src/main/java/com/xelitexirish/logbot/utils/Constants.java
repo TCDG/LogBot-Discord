16be1e39d4bc44b3eda2531b2104f5de5955962a
King - Frangu Vlad <kingdgrizzle@gmail.com>
2016-12-04 15:22:26 +0200

    Slight Updates, preparing for v1.4.0, also reverted the changes I've made for the Logger exMachina (Test bot on TCDG)

--- src/main/java/com/xelitexirish/logbot/utils/Constants.java
+++ src/main/java/com/xelitexirish/logbot/utils/Constants.java
@@ -2,8 +2,8 @@ package com.xelitexirish.logbot.utils;
 
 public class Constants {
 	
-	public static final String COMMAND_PREFIX = "/logex ";
-    public static final String BOT_VERSION = "1.3.1";
+	public static final String COMMAND_PREFIX = "/log ";
+    public static final String BOT_VERSION = "1.4.0";
 
     public static final String XELITEXIRISH_ID = "182210823630880768";
     public static final String KINGDGRIZZLE_ID = "139836912335716352";
